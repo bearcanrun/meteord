5dd11ff5adcaa82602a8d33aa0b6f642842c54de
Barry Low <barrylow@gmail.com>
2016-07-31 19:03:42 -0700

    Update for npm install

--- base/scripts/lib/build_app.sh
+++ base/scripts/lib/build_app.sh
@@ -8,6 +8,11 @@ BUNDLE_DIR=/tmp/bundle-dir
 cp -R /app $COPIED_APP_PATH
 cd $COPIED_APP_PATH
 
+if [ -e ./package.json ]; then
+  # install app deps, skip devDeps if package.json
+  npm i --production
+fi
+
 meteor build --directory $BUNDLE_DIR --server=http://localhost:3000
 
 cd $BUNDLE_DIR/bundle/programs/server/
